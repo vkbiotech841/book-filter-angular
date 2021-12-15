054d1fa5a2612ee5d93d3871eb6a3ea2e45a04dd
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-12-15 15:10:51 +0530

    improvement

--- src/app/base_api/base-api-urls.ts
+++ src/app/base_api/base-api-urls.ts
@@ -1,7 +1,5 @@
 import { environment } from './../../environments/environment';
-import { Injectable } from "@angular/core";
 
-@Injectable()
 export class BaseApiUrl {
 
     public static Books = environment.BookUrl;
